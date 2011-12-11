a86f6701da66bd645459769ebf2f01cdc6d8968e
AleksandraSkrzypczak <a.skrzy@gmail.com>
2011-12-12 00:36:15 +0100

    guziczek do wylogowywania

---
+++ src/Goodle/src/edu/goodle/prototype/client/CourseListPanel.java
@@ -0,0 +1,52 @@
+package edu.goodle.prototype.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.LogRecord;
+
+public class CourseListPanel {
+	private final GoodleServiceAsync goodleService;
+	private final Goodle goodle;
+	private VerticalPanel coursePanel = new VerticalPanel();
+	private static Logger logger = Logger.getLogger("");
+	private Boolean loaded;
+
+	public CourseListPanel(GoodleServiceAsync goodleService, Goodle goodle) {
+		this.goodleService = goodleService;
+		this.goodle = goodle;
+		this.loaded = false;
+	}
+
+	public void loadCourses (String sessionId) {
+		goodleService.getCourses(sessionId, new AsyncCallback<String>() {
+			public void onFailure(Throwable caught) {
+				logger.severe("getCourses: fail " + caught);
+			}
+			public void onSuccess(String result) {
+				logger.info("getCourses: ok");
+				Hyperlink h = new Hyperlink(result, "");
+				coursePanel.add(h);
+			}
+		});
+	}
+
+	public VerticalPanel getPanel(String sessionId) {
+		if (!loaded) {
+			loadCourses(sessionId);
+			loaded = true;
+		}
+		return coursePanel;
+	}
+
+	public void remove() {
+		RootPanel.get("courses").remove(coursePanel);
+		loaded = false;
+		coursePanel.clear();
+	}
+}
--- src/Goodle/src/edu/goodle/prototype/client/Goodle.java
+++ src/Goodle/src/edu/goodle/prototype/client/Goodle.java
@@ -2,9 +2,13 @@ package edu.goodle.prototype.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.logging.client.ConsoleLogHandler;
 import com.google.gwt.logging.client.FirebugLogHandler;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import java.util.logging.Level;
@@ -19,12 +23,16 @@ public class Goodle implements EntryPoint {
 
 	private LoginPanel lp = new LoginPanel(goodleService, this);
 	private CourseListPanel cp = new CourseListPanel(goodleService, this);
+	private Button logoutButton = new Button("Wyloguj");
 
 	public void onModuleLoad() {
-		if (sessionId == "")
+		if (sessionId == "") {
 			RootPanel.get("goodleLogin").add(lp.getPanel());
-		else
+			DOM.setElementAttribute(
+				DOM.getElementById("goodleLogin"), "style", "visibility:visible");
+		} else {
 			afterLogin(sessionId);
+		}
 	}
 
 	public void afterLogin(String result) {
@@ -33,5 +41,22 @@ public class Goodle implements EntryPoint {
 			DOM.getElementById("goodleLogin"), "style", "visibility:hidden");
 		logger.info("SessionID: "+result);
 		RootPanel.get("courses").add(cp.getPanel(sessionId));
+		RootPanel.get("goodleLogout").add(logoutButton);
+
+		logoutButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				goodleService.logoutUser(sessionId, new AsyncCallback<Void>() {
+					public void onFailure(Throwable caught) {
+						logger.severe("Logout failed." + caught);
+					}
+					public void onSuccess(Void v) {
+						RootPanel.get("goodleLogout").remove(logoutButton);
+						sessionId = "";
+						cp.remove();
+						onModuleLoad();
+					}
+				});
+			}
+		});
 	}
 }

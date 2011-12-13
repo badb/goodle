5f0ec33d21ab8ec5d6b69b3402fe4fb0422baeda
AleksandraSkrzypczak <a.skrzy@gmail.com>
2011-12-13 21:02:59 +0100

    brakujace klasy

---
+++ src/Goodle/src/edu/goodle/prototype/client/GoodlePanel.java
@@ -0,0 +1,21 @@
+package edu.goodle.prototype.client;
+
+import com.google.gwt.user.client.ui.Panel;
+
+public abstract class GoodlePanel {
+	private final GoodleServiceAsync goodleService;
+	private final Goodle goodle;
+
+	public GoodlePanel(GoodleServiceAsync goodleService, Goodle goodle) {
+		this.goodle = goodle;
+		this.goodleService = goodleService;
+	}
+
+	public Goodle getGoodle() {
+		return goodle;
+	}
+
+	public GoodleServiceAsync getGoodleService() {
+		return goodleService;
+	}
+}
---
+++ src/Goodle/src/edu/goodle/prototype/client/SearchPanel.java
@@ -0,0 +1,86 @@
+package edu.goodle.prototype.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SuggestBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.LogRecord;
+
+public class SearchPanel extends GoodlePanel {
+
+	private static Logger logger = Logger.getLogger("");
+
+	private HorizontalPanel searchPanel = new HorizontalPanel();
+	private VerticalPanel searchMainPanel = new VerticalPanel();
+
+	private SuggestBox searchBox = new SuggestBox(createCoursesOracle());
+	private Button searchButton = new Button("OK");
+	private Label searchLabel = new Label("Szukaj:");
+
+	private VerticalPanel searchResultPanel = new VerticalPanel();
+
+	public SearchPanel(GoodleServiceAsync goodleService, Goodle goodle) {
+		super(goodleService, goodle);
+
+		searchMainPanel.add(searchLabel);
+		searchPanel.add(searchBox);
+		searchPanel.add(searchButton);
+		searchMainPanel.add(searchPanel);
+
+		searchButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				searchResultPanel.clear();
+				getGoodleService().searchCourse(searchBox.getText(),
+					getGoodle().getSession(), new AsyncCallback<String>() {
+
+					public void onFailure(Throwable caught) {
+						logger.severe("Search failed." + caught);
+					}
+					public void onSuccess(String result) {
+						logger.info("Search ok: " + result);
+						searchResultPanel.add(new Hyperlink(result, result));
+						getGoodle().showSearchResultPanel(searchResultPanel);
+					}
+				});
+			}
+		});
+	}
+
+	public VerticalPanel getPanel() {
+		return searchMainPanel;
+	}
+
+	public void remove() {
+		RootPanel.get("goodleSearch").remove(searchMainPanel);
+	}
+
+	private MultiWordSuggestOracle createCoursesOracle() {
+		MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
+		oracle.add("AWERT");
+		oracle.add("BLA");
+		oracle.add("QWERTY");
+		
+		getGoodleService().getAllCourses(getGoodle().getSession(),
+			new AsyncCallback<String>() {
+
+			public void onFailure(Throwable caught) {
+				logger.severe("GetAllCourses failed." + caught);
+			}
+			public void onSuccess(String result) {
+				logger.info("GetAllCourses:" + result);
+			}
+		});
+		return oracle;
+	}
+}
